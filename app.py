b751347774a493a34c9f3eb0d1e47c01b972454b
roignantmathys-boop <roignantmathys@gmail.com>
2026-05-09 16:49:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,19 @@ st.set_page_config(
 )
 
 st.title("🏗️ Gestion Chantier BTP")
+
 st.subheader("Bienvenue")
 
-st.write("Cliquez sur un module pour l’ouvrir :")
+st.write("Cliquez sur un module :")
 
 col1, col2 = st.columns(2)
 
 with col1:
+
     if st.button("📁 Chantiers", use_container_width=True):
-        st.switch_page("pages/1_📁_Chantiers.py")
+        st.switch_page("pages/1_Chantiers.py")
 
 with col2:
+
     if st.button("♻️ Registre Déchets", use_container_width=True):
-        st.switch_page("pages/2_♻️_Registre_Déchets.py")
-
-st.divider()
-
-st.info("Les autres modules seront ajoutés ensuite : FDS, Devis, Vérifications, Dashboard.")
+        st.switch_page("pages/2_Registre_Dechets.py")
